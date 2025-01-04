aba6e77478bab415f469b4278aed1a18daed58e6
jkaufhold <julius.kaufhold1@gmail.com>
2025-01-04 04:49:11 +0100

    add db and tailwind

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,18 @@
 from flask import Flask
+import os
 
 def create_app():
     app=Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"]=os.getenv("DATABASE_URI")
+
+
+    from app.routes import main
+    app.register_blueprint(main)
+
+    from app.models import User,db
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
     return app
